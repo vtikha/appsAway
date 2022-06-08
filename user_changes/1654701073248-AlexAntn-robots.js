fc2f25179c19093b284ce829e33e87dfd35da5dc
GitHub Action <actions@github.com>
2022-06-08 15:18:56 +0000

    removing user_changes from appsAway

--- user_changes/1654701073248-AlexAntn-robots.js
+++
@@ -1,4 +0,0 @@
-//AlexAntn
-//robot-bazaar-files/robots/iCubErzelli02/1654701073248-iCubErzelli02-img.jpg 
-//&<h3>Field changed in robot: <ins>iCubErzelli02</ins></h3><ul><li>img: https://storage.googleapis.com/robot-bazaar-files/robots/iCubErzelli02/1654<del>69</del><b>70107</b>3<del>49</del>24<del>19</del><b>8</b>-iCubErzelli02-img.jpg</li></ul>
-db.robots.update ({_id: ObjectId("5fdbf1db9d8a3bc1ead6ee55")},{$set: { img: "https://storage.googleapis.com/robot-bazaar-files/robots/iCubErzelli02/1654701073248-iCubErzelli02-img.jpg"}});
