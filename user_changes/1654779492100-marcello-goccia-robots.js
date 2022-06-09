7d2f6556d8c563fffd87d56364ecd3d5131180f3
GitHub Action <actions@github.com>
2022-06-09 13:03:43 +0000

    removing user_changes from appsAway

--- user_changes/1654779492100-marcello-goccia-robots.js
+++
@@ -1,4 +0,0 @@
-//marcello-goccia
-//robot-bazaar-files/robots/iCubAberystwyth01/1654779492100-iCubAberystwyth01-img.jpg 
-//&<h3>Field changed in robot: <ins>iCubAberystwyth01</ins></h3><ul><li>img: https://storage.googleapis.com/robot-bazaar-files/robots/i<del>c</del><b>C</b>ub<del>_d</del><b>Ab</b>e<del>fa</del><b>rystwyth01/1654779492100-iC</b>u<del>l</del><b>bAberystwy</b>t<b>h01-img</b>.<b>j</b>p<del>n</del>g</li></ul>
-db.robots.update ({_id: ObjectId("5fdbf1db9d8a3bc1ead6ee4b")},{$set: { img: "https://storage.googleapis.com/robot-bazaar-files/robots/iCubAberystwyth01/1654779492100-iCubAberystwyth01-img.jpg"}});
